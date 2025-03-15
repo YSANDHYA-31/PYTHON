eedf318284dd59a6747ae7c7b5e89ada7e4cefa7
YSANDHYA-31 <yagatisandhya123@gmail.com>
2025-03-15 19:31:02 +0530

    Delete FILES directory

--- FILES/File2_seek().py
+++
@@ -1,9 +0,0 @@
-# using seek()
-
-# Open the file in read mode
-with open("test.txt", "r") as file:
-    file.seek(10)  # Move to the 10th index (starting from 0)
-    data = file.read()  # Read from index 10 onwards
-    print("File content from index 10 onwards:")
-    print(data)
-
--- FILES/File3_read.py
+++
@@ -1,8 +0,0 @@
-# read mode
-
-with open("sample.txt", "r") as file:
-    content = file.read()  # Read the entire file
-
-# Print the content
-print("📄 File Content:\n")
-print(content)
